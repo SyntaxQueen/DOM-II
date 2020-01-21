6888e4be75aeed4aa6b244b8b4bef085415b57ca
Kris <kristian.claire.101@gmail.com>
2020-01-20 17:55:21 -0800

    adds mouseenter and mouseleave events

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// #1: MouseOver on the nav links
+// #1: Mouseover Event
 document.querySelectorAll(".nav a").forEach(element => {
     element.addEventListener("mouseover", () => {
         element.style.backgroundColor = "red";
@@ -9,9 +9,22 @@ document.querySelectorAll(".nav a").forEach(element => {
     }, false);
 });
 
-// #2  Keydown 
+// #2  Keydown Event
 window.addEventListener("keydown", () => {
     const downText = document.querySelector(".logo-heading");
     downText.style.color = "blue";
     downText.textContent = "Why are eggs not very much into jokes? Because they could crack up!"
 });
+
+// #3 Mouseenter Event
+const busImg = document.querySelector(".intro img");
+
+busImg.addEventListener('mouseenter', () => {
+    busImg.style.transform = "scale(1.5)";
+    busImg.style.transition = "all 0.3s";
+})
+
+// #4 Mouseleave Event
+busImg.addEventListener('mouseleave', () => {
+    busImg.style.transform = "scale(1)";
+})
