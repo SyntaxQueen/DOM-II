8cdcffe808373042cf7924bb3341736d6ca89a83
Kris <kristian.claire.101@gmail.com>
2020-01-20 17:11:02 -0800

    adds mouseover event to nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// #1: MouseOver on the nav links
+document.querySelectorAll(".nav a").forEach(element => {
+    element.addEventListener("mouseover", () => {
+        element.style.backgroundColor = "red";
+
+    setTimeout(() => {
+        element.style.backgroundColor = "";
+    }, 200)
+    }, false);
+});
+
+// #2 
